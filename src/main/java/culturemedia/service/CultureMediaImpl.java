feb4e4a935306b9e54798e454507c7ae9de3a541
MulkePulver <juanandres.garcia.duarte@gmail.com>
2024-10-23 23:41:20 -0500

    R8-Adding–business-logic-to-the-service

--- src/main/java/culturemedia/service/CultureMediaImpl.java
+++ src/main/java/culturemedia/service/CultureMediaImpl.java
@@ -2,12 +2,17 @@ package culturemedia.service;
 
 import culturemedia.model.Video;
 import culturemedia.model.View;
+import culturemedia.repository.impl.VideoRepositoryImpl;
+import culturemedia.repository.impl.ViewsRepositoryImpl;
 
 import java.util.List;
 
 public class CultureMediaImpl implements CulturemediaService {
+    public CultureMediaImpl(VideoRepositoryImpl videoRepository, ViewsRepositoryImpl viewsRepository) {
+    }
+
     @Override
-    public List<Video> listarTodos() {
+    public List<Video> listAll() {
         return List.of();
     }
 
