a48f84074b35ff3990787807a4db6c1a09444675
MulkePulver <juanandres.garcia.duarte@gmail.com>
2024-11-06 19:22:18 -0500

    R10-Adding–controller

--- src/main/java/culturemedia/service/CulturemediaService.java
+++ src/main/java/culturemedia/service/CulturemediaService.java
@@ -6,7 +6,7 @@ import culturemedia.model.View;
 import java.util.List;
 
 public interface CulturemediaService {
-    List<Video> listAll();
+    List<Video> findAll();
     Video save(Video video);
     View save(View view);
 }
